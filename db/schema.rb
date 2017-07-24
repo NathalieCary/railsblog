ea527f3de4f71084a85f75d9f2484ed42fe06e5f
nathalie <nathalie19@live.co.za>
2017-07-25 11:50:23 +1200

    added article scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170723202152) do
+ActiveRecord::Schema.define(version: 20170724211005) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20170723202152) do
     t.datetime "published_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image_url"
   end
 
 end
